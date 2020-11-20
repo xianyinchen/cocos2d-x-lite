b45a2569302c5905e037f2551b165b4a6ddf0f5c
chenxianyin <420373550@qq.com>
2020-11-20 18:20:09 +0800

    switch to official version 4.9.0

--- cocos/platform/android/java/src/org/cocos2dx/lib/Cocos2dxDownloader.java
+++ cocos/platform/android/java/src/org/cocos2dx/lib/Cocos2dxDownloader.java
@@ -46,11 +46,11 @@ import java.util.concurrent.TimeUnit;
 // Rename package okhttp3 to org.cocos2dx.okhttp3
 // Github repo: https://github.com/PatriceJiang/okhttp/tree/cocos2dx-rename-3.12.x
 // and https://github.com/PatriceJiang/okio/tree/cocos2dx-rename-1.15.0
-import org.cocos2dx.okhttp3.Call;
-import org.cocos2dx.okhttp3.Callback;
-import org.cocos2dx.okhttp3.OkHttpClient;
-import org.cocos2dx.okhttp3.Request;
-import org.cocos2dx.okhttp3.Response;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class Cocos2dxDownloader {
 
